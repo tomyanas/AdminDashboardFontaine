d6118b027f2c799ba212a4dcf709f5a2769525e9
anastasiotomas@gmail.com <anastasiotomas@gmail.com>
2022-02-03 19:11:52 -0300

    feat(login & signup):ready

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import HomePage from "../Pages/HomePage";
 
 export const Routing = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>holas</div>} />
+      <Route path="/" element={<HomePage/>} />
     </Routes>
   );
 };
